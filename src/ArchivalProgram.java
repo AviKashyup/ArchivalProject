bfd34d30b18d7ee5bff684d27c434f0315fd3559
AbhiSharmaCodes <87431770+AbhiSharmaCodes@users.noreply.github.com>
2022-09-23 11:43:01 -0400

    Update ArchivalProgram.java

--- src/ArchivalProgram.java
+++ src/ArchivalProgram.java
@@ -1,5 +1,5 @@
 public class ArchivalProgram {
     public static void main(String[] args) {
-        System.out.println("It is the main class");
+        System.out.println("No It IS the main class");
     }
 }
